5c5c284f7c684335c2633e583558bc8373f64510
katerine chen <chen.katerine@gmail.com>
2017-10-09 13:50:08 +0800

    ex5 modi

--- MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1,btn2,btn3,btn4;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.btn1);
+        btn2=(Button)findViewById(R.id.btn2);
+        btn3=(Button)findViewById(R.id.btn3);
+        btn4=(Button)findViewById(R.id.btn4);
     }
 
     public void click1(View v){
