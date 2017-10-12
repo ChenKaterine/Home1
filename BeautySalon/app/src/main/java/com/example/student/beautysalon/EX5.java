cd363a5a4ec63ba69698bc1f6fc89bda93bbda84
katerine chen <chen.katerine@gmail.com>
2017-10-12 16:58:56 +0800

    modify

--- BeautySalon/app/src/main/java/com/example/student/beautysalon/EX5.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/EX5.java
@@ -1,7 +1,10 @@
 package com.example.student.beautysalon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class EX5 extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class EX5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex5);
+
+
+
+
+    }
+
+    public void click9(View view) {
+        Uri uri = Uri.parse("tel://0227735243");
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+
+    public void click8(View view) {
+        Uri uri = Uri.parse("geo:25.0417025,121.5504265?zoom=15");
+        //Uri uri = Uri.parse("geo:0,0?q=巨匠電腦東區認證");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
+    public void click7(View view) {
+        
     }
 }
