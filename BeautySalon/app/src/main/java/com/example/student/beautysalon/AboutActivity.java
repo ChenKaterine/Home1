bec020872e16d09d6e2c9748c6628787b4f48587
katerine chen <chen.katerine@gmail.com>
2017-10-17 09:09:27 +0800

    modify web

--- BeautySalon/app/src/main/java/com/example/student/beautysalon/AboutActivity.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/AboutActivity.java
@@ -1,13 +1,23 @@
 package com.example.student.beautysalon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
-
+    WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutactivity);
+        wv = (WebView) findViewById(R.id.webView);
+        //設定網路連線true
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebChromeClient(new WebChromeClient());
+        Intent it = getIntent();
+        wv.clearCache(true);
+        wv.loadUrl("https://android-5bfc6.firebaseapp.com/");
     }
 }
--- BeautySalon/app/src/main/java/com/example/student/beautysalon/NewsActivity.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/NewsActivity.java
@@ -1,24 +1,15 @@
 package com.example.student.beautysalon;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
 
 public class NewsActivity extends AppCompatActivity {
-    WebView wv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsactivity);
-        wv = (WebView) findViewById(R.id.webView);
-        //設定網路連線true
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.setWebChromeClient(new WebChromeClient());
-        Intent it = getIntent();
-        wv.clearCache(true);
-        wv.loadUrl("https://android-5bfc6.firebaseapp.com/");
+
 
     }
 }
