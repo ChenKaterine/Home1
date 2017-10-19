7b17360d75e1240a1a02ad5c67d620e269e54ac5
katerine chen <chen.katerine@gmail.com>
2017-10-19 16:49:08 +0800

    email,line

--- BeautySalon/app/src/main/java/com/example/student/beautysalon/ContactActivity.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/ContactActivity.java
@@ -33,5 +33,22 @@ public class ContactActivity extends AppCompatActivity {
 
     public void clickemail(View view) {
 
+        Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject of email");
+        intent.putExtra(Intent.EXTRA_TEXT, "Body of email");
+        intent.setData(Uri.parse("mailto:chen.katerine@gmail.com")); // or just "mailto:" for blank
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+        startActivity(intent);
+    }
+
+    public void clicktalk(View view) {
+        final String PACKAGE_NAME = "jp.naver.line.android";
+        final String CLASS_NAME = "jp.naver.line.android.activity.selectchat.SelectChatActivity";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setClassName(PACKAGE_NAME, CLASS_NAME);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello World!!");
+        startActivity(intent);
     }
 }
