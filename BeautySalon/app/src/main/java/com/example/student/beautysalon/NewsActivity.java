cf87f997b8350f499eec65ebba3124cfad48864a
katerine chen <chen.katerine@gmail.com>
2017-10-19 11:20:43 +0800

    modify news view

--- BeautySalon/app/src/main/java/com/example/student/beautysalon/NewsActivity.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/NewsActivity.java
@@ -3,21 +3,104 @@ package com.example.student.beautysalon;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class NewsActivity extends AppCompatActivity {
-    WebView wv1;
+    ListView lv;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsactivity);
-        wv1 = (WebView) findViewById(R.id.webView1);
-        wv1.getSettings().setJavaScriptEnabled(true);
-        wv1.setWebChromeClient(new WebChromeClient());
-        Intent it = getIntent();
-        String url = it.getStringExtra("link");
-        wv1.loadUrl(url);
 
+
+        lv=(ListView)findViewById(R.id.listView);
+        new Thread(){
+            @Override
+            public void run()
+            {
+                URL url = null;
+                try {
+                    url = new URL("https://style.udn.com/rss/news/2002/8054/8079?ch=style");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream;
+                    inputStream = conn.getInputStream();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    final StringBuilder sb = new StringBuilder();
+                    String str;
+                    while ((str = br.readLine()) != null)
+                    {
+                        sb.append(str);
+                    }
+                    String result = sb.toString();
+                    final MyDataHandler dataHandler = new MyDataHandler();
+                    SAXParserFactory spf = SAXParserFactory.newInstance();
+                    SAXParser sp = spf.newSAXParser();
+                    XMLReader xr = sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(result)));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<String> forAdapter = new ArrayList<String>();
+                            for (RSSNewsItem i : dataHandler.data)
+                            {
+                                forAdapter.add(i.title);
+                            }
+                            adapter = new ArrayAdapter<String>(NewsActivity.this, android.R.layout.simple_list_item_1, forAdapter);
+                            lv.setAdapter(adapter);
+                            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    Intent it = new Intent( NewsActivity.this,NewsDetail.class);
+                                    it.putExtra("link", dataHandler.data.get(i).link);
+                                    startActivity(it);
+                                }
+                            });
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }.start();
     }
 }
---
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/NewsDetail.java
@@ -0,0 +1,23 @@
+package com.example.student.beautysalon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class NewsDetail extends AppCompatActivity {
+    WebView wv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+        WebView wv1;
+        wv1 = (WebView) findViewById(R.id.webView1);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setWebChromeClient(new WebChromeClient());
+        Intent it = getIntent();
+        String url = it.getStringExtra("link");
+        wv1.loadUrl(url);
+    }
+}
