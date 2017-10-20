16784a3fd338c75cd5611f1071f0b381090240ca
katerine chen <chen.katerine@gmail.com>
2017-10-20 17:14:04 +0800

    String modi

--- BeautySalon/app/src/main/java/com/example/student/beautysalon/ContactActivity.java
+++ BeautySalon/app/src/main/java/com/example/student/beautysalon/ContactActivity.java
@@ -5,10 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 
 public class ContactActivity extends AppCompatActivity {
-    ImageView image3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
